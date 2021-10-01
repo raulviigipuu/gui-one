386b7fa4c7b7774fbd3afbdae51744396fc5ffaf
Raul Viigipuu <raul.viigipuu@smit.ee>
2021-10-01 16:17:39 +0300

    code

---
+++ src/main/groovy/dev/App.groovy
@@ -0,0 +1,39 @@
+package dev
+
+import javax.swing.*
+import com.formdev.flatlaf.FlatLightLaf;
+
+class App {
+
+    static void main(String[] args) {
+
+
+        new App().initGUI()
+    }
+
+    void lafList() {
+        UIManager.LookAndFeelInfo[] lookAndFeels = UIManager.getInstalledLookAndFeels();
+
+        for (UIManager.LookAndFeelInfo lookAndFeel : lookAndFeels) {
+            println "Name      = " + lookAndFeel.getName()
+            println "ClassName = " + lookAndFeel.getClassName()
+        }
+    }
+
+    void initGUI() {
+        //FlatLightLaf.setup();
+        JFrame frame = new JFrame("GUI one")
+        try {
+            UIManager.setLookAndFeel("com.formdev.flatlaf.FlatLightLaf")
+        } catch(Exception e) {
+            e.printStackTrace()
+        }
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
+        frame.setSize(300,300)
+        JLabel label = new JLabel("asd asd")
+        JButton button = new JButton("Press")
+        frame.getContentPane().add(label)
+        frame.getContentPane().add(button)
+        frame.setVisible(true)
+    }
+}
