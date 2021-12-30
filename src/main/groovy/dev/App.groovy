735a578477a8702d8b474f20baa94f64a7f010c1
Raul Viigipuu <raul@viigipuu.ee>
2021-12-30 02:45:18 +0200

    bin out, format code

--- src/main/groovy/dev/App.groovy
+++ src/main/groovy/dev/App.groovy
@@ -1,39 +1,38 @@
 package dev
 
 import javax.swing.*
-import com.formdev.flatlaf.FlatLightLaf;
+import com.formdev.flatlaf.FlatLightLaf
 
 class App {
 
     static void main(String[] args) {
-
-
         new App().initGUI()
     }
 
     void lafList() {
-        UIManager.LookAndFeelInfo[] lookAndFeels = UIManager.getInstalledLookAndFeels();
+        UIManager.LookAndFeelInfo[] lookAndFeels = UIManager.getInstalledLookAndFeels()
 
         for (UIManager.LookAndFeelInfo lookAndFeel : lookAndFeels) {
-            println "Name      = " + lookAndFeel.getName()
-            println "ClassName = " + lookAndFeel.getClassName()
+            println 'Name      = ' + lookAndFeel.name()
+            println 'ClassName = ' + lookAndFeel.className()
         }
     }
 
     void initGUI() {
         //FlatLightLaf.setup();
-        JFrame frame = new JFrame("GUI one")
+        JFrame frame = new JFrame('GUI one')
         try {
-            UIManager.setLookAndFeel("com.formdev.flatlaf.FlatLightLaf")
-        } catch(Exception e) {
+            UIManager.setLookAndFeel('com.formdev.flatlaf.FlatLightLaf')
+        } catch (Exception e) {
             e.printStackTrace()
         }
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
-        frame.setSize(300,300)
-        JLabel label = new JLabel("asd asd")
-        JButton button = new JButton("Press")
+        frame.setSize(300, 300)
+        JLabel label = new JLabel('asd asd')
+        JButton button = new JButton('Press')
         frame.getContentPane().add(label)
         frame.getContentPane().add(button)
         frame.setVisible(true)
     }
+
 }
