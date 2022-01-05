2d8529c8ffda313d1a4c18f5c6cb9379aab324d6
Raul Viigipuu <raul@viigipuu.ee>
2022-01-05 04:36:21 +0200

    gui init

--- src/main/groovy/dev/App.groovy
+++ src/main/groovy/dev/App.groovy
@@ -3,21 +3,42 @@ package dev
 import javax.swing.*
 import com.formdev.flatlaf.FlatLightLaf
 
+import java.awt.GridBagConstraints
+import java.awt.GridBagLayout
+
 class App {
 
+    public static int clickCount = 0
+
     static void main(String[] args) {
-        new App().initGUI()
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            void run() {
+                initGUI()
+            }
+        })
     }
 
-    void initGUI() {
-        FlatLightLaf.setup();
+    static void initGUI() {
+        FlatLightLaf.setup()
         JFrame frame = new JFrame('GUI one')
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
         frame.setSize(300, 300)
-        JLabel label = new JLabel('asd asd')
-        JButton button = new JButton('Press')
-        frame.getContentPane().add(label)
-        frame.getContentPane().add(button)
+        JButton btnClickCount = new JButton('Press me')
+        JLabel lblClickCount = new JLabel('Click count: 0')
+        btnClickCount.addActionListener(actionEvent -> {
+            clickCount++
+            lblClickCount.setText("Click count: $clickCount")
+        })
+        JPanel panel = new JPanel(new GridBagLayout())
+        GridBagConstraints constraints = new GridBagConstraints()
+        constraints.gridx = 0
+        constraints.gridy = 0
+        panel.add(btnClickCount, constraints)
+        constraints.gridx = 0
+        constraints.gridy = 1
+        panel.add(lblClickCount, constraints)
+        frame.getContentPane().add(panel)
         frame.setVisible(true)
     }
 
