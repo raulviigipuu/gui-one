43f291bd2fb74b5f3c61c71a09fcbc29f0071df0
Raul Viigipuu <raul@viigipuu.ee>
2022-01-05 04:10:47 +0200

    cleanup

--- src/main/groovy/dev/App.groovy
+++ src/main/groovy/dev/App.groovy
@@ -9,23 +9,9 @@ class App {
         new App().initGUI()
     }
 
-    void lafList() {
-        UIManager.LookAndFeelInfo[] lookAndFeels = UIManager.getInstalledLookAndFeels()
-
-        for (UIManager.LookAndFeelInfo lookAndFeel : lookAndFeels) {
-            println 'Name      = ' + lookAndFeel.name()
-            println 'ClassName = ' + lookAndFeel.className()
-        }
-    }
-
     void initGUI() {
-        //FlatLightLaf.setup();
+        FlatLightLaf.setup();
         JFrame frame = new JFrame('GUI one')
-        try {
-            UIManager.setLookAndFeel('com.formdev.flatlaf.FlatLightLaf')
-        } catch (Exception e) {
-            e.printStackTrace()
-        }
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
         frame.setSize(300, 300)
         JLabel label = new JLabel('asd asd')
